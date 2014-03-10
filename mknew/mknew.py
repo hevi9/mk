1d05ec3ce6e583884212febaff266fc0d3636d8d
hevi <hevi@lut.fi>
2014-03-10 14:43:50 +0200

    make multiple files

--- mknew/mknew.py
+++ mknew/mknew.py
@@ -10,6 +10,7 @@ import os
 j = os.path.join
 from fnmatch import fnmatch
 import jinja2
+import sys
 
 TMPL_DIR = "Templates"
 
@@ -17,6 +18,7 @@ TMPL_DIR = "Templates"
 _entries = dict()  # name as str -> entry as Entry
 _files = dict()
 _jenv = None
+
 
 def is_ignored(name):
   ignored = [
@@ -30,7 +32,7 @@ def is_ignored(name):
     if fnmatch(name, pat):
       return True
   return False
-  
+
 
 def find_tmpl_roots():
   """ Find template directory roots. Resolving priority order.
@@ -53,97 +55,62 @@ def find_tmpl_roots():
   return result2
 
 
-def find_entries(roots, entries):
-  """ entries as dict of name as str to entry as Entry is a
-  collection of template things to instantiate. entries dict is
-  filled by this function. """
-  for root in roots:
-    for name in os.listdir(root):
-      if is_ignored(name):
-        continue
-      if name not in entries:
-        entries[name] = Entry(root,name)
-  return entries 
-
-
-class Entry:
-  def __init__(self, root, name):
-    self._root = root
-    self._name = name
-    self._files = None
-
-  @property
-  def path(self):
-    return j(self._root, self._name)
-
-  @property
-  def files(self):
-    if not self._files:
-      self._files = list()
-      if os.path.isfile(self.path):
-        self._files.append(self._name)
-      for top, dirnames, filenames in os.walk(self.path):
-        for filename in filenames:
-          path = j(top,filename)[len(self._root)+1:]
-          self._files.append(path)
-    return self._files
-
-
-
-def show_entries(entries, match=None):
-  """ """
-  print("Templates")
-  if match is None:
-    for name in sorted(entries):
-      entry = entries[name]
-      print(" *", name, len(entry.files))
-  else:
-    for name in sorted(entries):
-      if name.find(match) > -1:
-        print(" *",name)
-
-ARGS = argparse.ArgumentParser()
-ARGS.add_argument("tmpl", nargs="?",                  
-                  help="thing (file,dir,project,..) to create")
-ARGS.add_argument("dest", nargs="?",                  
-                  help="thing (file,dir,project,..) to create")
-ARGS.add_argument("-d", "--debug", action="store_true",
-                  help="set debugging on")
-ARGS.add_argument("-f","--frm",nargs=1,
-                  help="Template name to instantiate from")
-
-
-def instantiate_file(entry, file, path):
-  jinja2
-
-
-def instantiate(entry, thing):
-  for file in entry.files:
-    path = file.split(os.sep)
-    path[0] = thing
-    path = os.sep.join(path)
-    D("%s => %s", file, path)
-    instantiate_file(entry, file, path)
-
-
 def updatefunc():
   return True
+
 
 def load_tmpl(name):
   file = _files[name]
   with open(file.apath) as fo:
-    source = fo.read()  
+    source = fo.read()
   return source, file.apath, updatefunc
 
-class File(object):
-    
+
+class Thing(object):
+
   def __init__(self, root, path):
     self._root = root
     self._path = path
 
   @property
+  def path(self):
+    return self._path
+
+  @property
+  def root(self):
+    return self._root
+
+  @property
   def apath(self):
     return j(self._root, self._path)
+
+
+class File(Thing):
+
+  def __init__(self, root, path):
+    super().__init__(root, path)
+    self.files = [self]
+
+  def show(self):
+    return "F {}".format(self.path)
+
+
+class Dir(Thing):
+
+  def __init__(self, root, path):
+    super().__init__(root, path)
+    self.files = list()
+
+  def show(self):
+    return "D {} {}".format(self.path, len(self.files))
+
+
+def _add_path_to_dirs(files, thing):
+  dir = os.path.dirname(thing.path)
+  while len(dir):
+    files[dir].files.append(thing)
+    dir = os.path.dirname(dir)
+
 
 def find_files(roots, files_store):
   for root in roots:
@@ -151,16 +118,67 @@ def find_files(roots, files_store):
       for dir in list(dirs):
         if is_ignored(dir):
           dirs.remove(dir)
+          continue
+        path = j(top, dir)[len(root) + 1:]
+        files_store[path] = Dir(root, path)
+        _add_path_to_dirs(files_store, files_store[path])
       for file in files:
         if is_ignored(file):
           continue
-        path = j(top,file)[len(root)+1:]
-        files_store[path] = File(root, path) 
+        path = j(top, file)[len(root) + 1:]
+        files_store[path] = File(root, path)
+        _add_path_to_dirs(files_store, files_store[path])
 
-def make_file(tmpl, dest):
-  jtmpl = _jenv.get_template(tmpl)
-  with open(dest,"w") as fo:
-    fo.write(jtmpl.render({}))
+
+def show_files(files, match=None):
+  print("Templates ({}):".format(len(files)))
+  if match is None:
+    match = "*"
+  else:
+    match = "*" + match + "*"
+  for file in sorted(files):
+    if fnmatch(file, match):
+      print(" ", files[file].show())
+
+
+def make_file(thing, dest):
+  print("mk", thing.path, "=>", dest)
+  if isinstance(thing, Dir):
+    os.mkdir(dest)
+  else:
+    jtmpl = _jenv.get_template(thing.path)
+    with open(dest, "w") as fo:
+      fo.write(jtmpl.render({}))
+
+
+def make_files(files, tmpl, dest):
+  thing = files[tmpl]
+  make_set = list()
+  if isinstance(thing, Dir):
+    make_set.append((thing, dest))
+  for thing in thing.files:
+    path = thing.path.split(os.sep)
+    path[0] = dest
+    path = os.sep.join(path)
+    make_set.append((thing, path))
+  ## check is dest files exists
+  for thing, path in make_set:
+    if os.path.exists(path):
+      print(path, "already existing, not making files")
+      sys.exit(1)
+  ## make
+  for thing, path in make_set:
+    make_file(thing, path)
+
+
+ARGS = argparse.ArgumentParser()
+ARGS.add_argument("tmpl", nargs="?",
+                  help="thing (file,dir,project,..) to create")
+ARGS.add_argument("dest", nargs="?",
+                  help="thing (file,dir,project,..) to create")
+ARGS.add_argument("-d", "--debug", action="store_true",
+                  help="set debugging on")
+
 
 def main():
   args = ARGS.parse_args()
@@ -170,13 +188,16 @@ def main():
   global _jenv
   _jenv = jinja2.Environment(loader=jinja2.FunctionLoader(load_tmpl))
   roots = find_tmpl_roots()
-  find_files(roots,_files)
+  find_files(roots, _files)
   if args.tmpl not in _files:
-    for file in sorted(_files):
-      print(file)
+    show_files(_files, args.tmpl)
+    sys.exit(3)
   else:
-    make_file(args.tmpl, args.dest)
+    if args.dest is None:
+      args.dest = args.tmpl
+    make_files(_files, args.tmpl, args.dest)
   D("done.")
+  sys.exit(0)
 
 if __name__ == "__main__":
   main()
