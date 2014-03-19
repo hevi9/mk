105e860ce8b52c09aaca798dc9f826bba0b563a5
hevi <hevi@lut.fi>
2014-03-19 15:04:29 +0200

    added TODO.py

---
+++ TODO.py
@@ -0,0 +1,6 @@
+# mknew future features
+# TODO.py because import of todo items into eclipse
+
+# TODO: binary/text file detection and handling
+# TODO: bash completion support
+# TODO: -e flag and $EDITOR env to edit text template files 
