929bba8fdd5f39bd7603eeb7c4c4f0d86b92ab90
Petri Heinilä <petri.heinila@visma.com>
2021-01-08 15:58:55 +0200

    fix tests

--- test/test_source.py
+++ test/test_source.py
@@ -2,22 +2,27 @@ from ruamel.yaml.scanner import ScannerError
 
 import pytest
 import mk.source
+from mk.location import Location
 
 # noinspection PyUnresolvedReferences
 from .fixtures import mkprimary, mkerror, mkroots
 
 
-def test_primary_source(mkprimary):
-    for source in mk.source.make_sources_from_file_yaml(*mkprimary):
+def test_primary_source(mkroots):
+    path_root, path_rel = mkroots["base"]["primary.mk.yaml"]
+    for source in mk.source.make_sources_from_file_yaml(
+        Location(path_root=path_root, path_rel=path_rel)
+    ):
         assert source.source in ("primary_file", "other_source")
         assert str(source.location.path_abs).endswith("primary.mk.yaml")
         assert type(source.make) is list
 
 
 def test_error_1(mkroots):
+    path_root, path_rel = mkroots["errors"]["error1.mk.yaml"]
     with pytest.raises(ScannerError) as ex:
         for _ in mk.source.make_sources_from_file_yaml(
-            *mkroots["errors"]["error1.mk.yaml"]
+            Location(path_root=path_root, path_rel=path_rel)
         ):
             pass
     msg = str(ex.value)
