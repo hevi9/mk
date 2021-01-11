04bfd9ed97915231020ff9bcd557d38dfc0ea97c
Petri Heinilä <hevi00@gmail.com>
2021-01-12 00:33:08 +0200

    added Root in testing

--- test/fixtures/mkroot.py
+++ test/fixtures/mkroot.py
@@ -10,6 +10,11 @@ SOURCE_PRIMARY = """
 - source: other_source
   make:
     - echo other source
+- source: combined
+  uses:
+    - primary_file
+    - other_source
+    
 """.strip()
 
 SOURCE_ERROR_1 = """
@@ -25,34 +30,43 @@ al dkaswdkas - SE
 """.strip()
 
 
-def have(path_root: Path, path_rel: Union[Path, str], text: str) -> Tuple[Path, Path]:
-    path_rel = Path(path_rel)
-    path_abs = path_root / path_rel
-    path_abs.parent.mkdir(parents=True, exist_ok=True)
-    with open(path_abs, "w") as fo:
-        fo.write(text)
-    return path_root, path_rel
+class Root:
+    def __init__(self, path_root: Path):
+        self.path_root = path_root
+
+    def have(self, path_rel: Union[Path, str], text: str) -> Tuple[Path, Path]:
+        path_rel = Path(path_rel)
+        path_abs = self.path_root / path_rel
+        path_abs.parent.mkdir(parents=True, exist_ok=True)
+        with open(path_abs, "w") as fo:
+            fo.write(text)
+        return self.path_root, path_rel
+
+
+@pytest.fixture(scope="session")
+def mkroot(tmp_path_factory) -> Root:
+    return Root(tmp_path_factory.mktemp("root"))
 
 
 @pytest.fixture(scope="session")
 def mkroots(tmp_path_factory) -> Dict:
-    root_base = tmp_path_factory.mktemp("base")
-    root_other = tmp_path_factory.mktemp("other")
-    root_errors = tmp_path_factory.mktemp("errors")
+    root_base = Root(tmp_path_factory.mktemp("base"))
+    root_other = Root(tmp_path_factory.mktemp("other"))
+    root_errors = Root(tmp_path_factory.mktemp("errors"))
     return {
         "base": {
             ".": root_base,
-            "primary.mk.yaml": have(root_base, "primary.mk.yaml", SOURCE_PRIMARY),
-            ".mk.yaml": have(root_base, ".mk.yaml", SOURCE_PRIMARY),
+            "primary.mk.yaml": root_base.have("primary.mk.yaml", SOURCE_PRIMARY),
+            ".mk.yaml": root_base.have(".mk.yaml", SOURCE_PRIMARY),
             "prj": {
-                ".mk.yaml": have(root_base, "prj/.mk.yaml", SOURCE_PRIMARY),
+                ".mk.yaml": root_base.have("prj/.mk.yaml", SOURCE_PRIMARY),
             },
         },
         "other": {".": root_other},
         "errors": {
             ".": root_errors,
-            "error1.mk.yaml": have(root_errors, "error1.mk.yaml", SOURCE_ERROR_1),
-            "error2.mk.yaml": have(root_errors, "error2.mk.yaml", SOURCE_ERROR_2),
+            "error1.mk.yaml": root_errors.have("error1.mk.yaml", SOURCE_ERROR_1),
+            "error2.mk.yaml": root_errors.have("error2.mk.yaml", SOURCE_ERROR_2),
         },
     }
 
